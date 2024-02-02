86e9e181267bd563994fc9181e93aba337a9ea6c
Paul Formby <paulformby@Pauls-MacBook-Air.local>
2024-02-01 16:42:02 -0800

    build navigation bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,18 @@
 import React from 'react';
-// import PhotoListItem from './components/PhotoListItem'; 
 import PhotoList from './components/PhotoList';
 import './App.scss';
-import TopicList from './components/TopicList';
-
-
-
-/* const data = new Array(3).fill().map((_, index) => ({
-  ...sampleDataForPhotoListItem,
-  id: index + 1,
-})); */
+import TopNavigationBar from './components/TopNavigationBar';
 
 
 const App = () => {
   return (
     <div className="App">
-     {/* { Array.from(Array(3)).map((_, index) => <PhotoListItem key={index}/>) } */}
-     <PhotoList/>
-     <TopicList/>
+  
+     <TopNavigationBar />
+     <PhotoList />
     </div>
   );
 };
 
-// Export the App component as the default export
+
 export default App;
