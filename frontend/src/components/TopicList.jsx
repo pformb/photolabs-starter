a3748f366b23eaa3ef2174eda39682b7de583ebc
Paul Formby <paulformby@Pauls-MacBook-Air.local>
2024-02-01 17:26:24 -0800

    add mock data

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -1,30 +1,12 @@
 import React from "react";
-
 import "../styles/TopicList.scss";
 import TopicListItem from './TopicListItem';
-
-const sampleDataForTopicList = [
-  {
-    id: "1",
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: "2",
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: "3",
-    slug: "topic-3",
-    title: "People",
-  },
-];
+import topics from '../mocks/topics.js';
 
 const TopicList = () => {
   return (
     <div className="top-nav-bar__topic-list">
-      {sampleDataForTopicList.map((topicData) => (
+      {topics.map((topicData) => (
         <TopicListItem key={topicData.id} topic={topicData} />
       ))}
     </div>
